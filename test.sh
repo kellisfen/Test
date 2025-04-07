98503eae12753e6161061648e990ccf8c10d49de
Алексей <aleksei862@gmail.com>
2025-04-07 23:26:24 +1000

    Добавлена функция system_info для вывода системной информации

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Это тестовый скрипт в ветке dev"
-echo "Версия 1.1"
+echo "Версия 1.2"
 
 # Простая функция
 hello() {
@@ -13,6 +13,15 @@ get_date() {
     echo "Текущая дата: $(date)"
 }
 
+# Функция для проверки системной информации
+system_info() {
+    echo "Информация о системе:"
+    echo "OS: $(uname -s)"
+    echo "Имя хоста: $(hostname)"
+    echo "Свободная память: $(free -h | grep Mem | awk '{print $4}')"
+}
+
 # Вызов функций
 hello
 get_date
+system_info
